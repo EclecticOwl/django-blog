01cd603b1c72e1601c8f70d1af5896e5237b72a1
Michael Tillman <mnamllit@gmail.com>
2022-03-27 18:24:32 -0500

    Added a user creation form to be able to register users.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+user = User
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+        
+    context = {}
+    return render(request, 'register.html', context)
+
