ddb8415b1b136f8a6e889d9b2672e8c8a96d1ab6
Michael Tillman <mnamllit@gmail.com>
2022-03-27 15:55:52 -0500

    Added a Profile model that uses a one to one field to translate the auth user model to the Profile.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=300, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
