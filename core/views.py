60c6356e716c409dfb51162e1f7591dcbf12e2a2
Michael Tillman <mnamllit@gmail.com>
2022-03-27 19:15:26 -0500

    Added login functionality and login page."

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-user = User
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -13,12 +12,31 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
             form.save()
             return redirect('index')
 
     else:
         form = UserCreationForm()
-        
+
     context = {}
     return render(request, 'register.html', context)
 
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print('success')
+
+            return redirect('index')
+        else:
+            print('Invalid')
+
+    
+    context = {}
+    return render(request, 'sign-in.html', context)
