b5e7f1df44afe8f493469c0d19dbcfdec6fc0308
Michael Tillman <mnamllit@gmail.com>
2022-03-29 21:09:02 -0500

    Created a post model for handling profile posts.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import Profile
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    description = models.CharField(max_length=100)
+    content = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.description
