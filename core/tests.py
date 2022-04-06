2c2babdd1c8193baf4d5027e6803d7934ded2ef9
Michael Tillman <mnamllit@gmail.com>
2022-04-05 20:51:00 -0500

    Added a test to check whether django signal function is correctly transferring the one to one relationship and the username to the profile model.

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,29 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+from core.models import Profile
+
+class UserToProfileModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            username='bob',
+            password='kjlekjxlKe13i'
+        )
+    
+    def test_one_to_one_rel(self):
+        profile = Profile.objects.get(id=1)
+        self.assertEqual(self.user.id, profile.user.id)
+    
+    def test_django_sig_username_profile(self):
+        profile = Profile.objects.get(id=1)
+        self.assertEqual(self.user.username, profile.user.username)
+    
+        
+
+
 class IndexTest(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
