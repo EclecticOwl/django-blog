b33db46c007dc3c70b8a685bafec84d7dc1cdd1a
Michael Tillman <mnamllit@gmail.com>
2022-04-05 19:51:41 -0500

    Added testing for the index function based view.

--- core/tests.py
+++ core/tests.py
@@ -1,2 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
+class IndexTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_access_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
