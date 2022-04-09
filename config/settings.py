f3c3c64163880271f9a5dd6f5169d84167bdbcf0
Michael Tillman <mnamllit@gmail.com>
2022-04-08 23:06:28 -0500

    Added a development key so testing can function on workflows.

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG == True:
+    SECRET_KEY = 'kdajldk#ldiu9DD/'
+else:
+    SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
