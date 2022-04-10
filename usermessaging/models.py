3e7f8b92888bc159e487439a46b9bc2eca4ead96
Michael Tillman <mnamllit@gmail.com>
2022-04-10 10:04:39 -0500

    Added a subject field to the messaging model. Also added some styling to the messaging app.

--- usermessaging/models.py
+++ usermessaging/models.py
@@ -4,6 +4,7 @@ from core.models import Profile
 class Message(models.Model):
     sender = models.ForeignKey(Profile, related_name='sender', on_delete=models.SET_NULL, null=True)
     receiver = models.ForeignKey(Profile, related_name='receiver', on_delete=models.SET_NULL, null=True)
+    subject = models.CharField(max_length=100)
     content = models.TextField(max_length=400)
     is_read = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
