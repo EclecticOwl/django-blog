681f48c829ed53782d4f57e8f090f7f8f63313e7
Michael Tillman <mnamllit@gmail.com>
2022-04-10 02:45:46 -0500

    Added an app for user-to-user messaging.

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
     'core',
     'posts',
+    'messages',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('core.urls')),
-
     path('posts/', include('posts.urls')),
+    path('messages/', include('messages.urls')),
+    
 ]
