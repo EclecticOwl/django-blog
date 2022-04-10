8a396fbc99f17615cf3ade6071e15fbf00dc0dbc
Michael Tillman <mnamllit@gmail.com>
2022-04-10 08:27:42 -0500

    Added an outbox and a detail page for the inbox and outbox.

--- usermessaging/urls.py
+++ usermessaging/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('inbox/', views.message_inbox, name='message_inbox'),
-    path('user/<int:id>/', views.message_detail, name='message_detail'),
+    path('outbox/', views.message_outbox, name='message_outbox'),
+    path('inbox/<int:id>/', views.message_detail_inbox, name='message_detail_inbox'),
+    path('outbox/<int:id>/', views.message_detail_outbox, name='message_detail_outbox'),
 ]
--- usermessaging/views.py
+++ usermessaging/views.py
@@ -8,11 +8,24 @@ def message_inbox(request):
     inbox = Message.objects.filter(receiver=user)
 
     context = {'inbox': inbox}
-    return render(request, 'messages.html', context)
+    return render(request, 'messages_inbox.html', context)
 
-def message_detail(request, id):
+def message_outbox(request):
+    user = request.user.profile
+    outbox = Message.objects.filter(sender=user)
+
+    context = {'outbox': outbox}
+    return render(request, 'messages_outbox.html', context)
+
+def message_detail_inbox(request, id):
     message = request.user.profile.receiver.get(id=id)
 
 
     context = {'message': message}
-    return render(request, 'user_message.html', context)
+    return render(request, 'user_inbox.html', context)
+
+def message_detail_outbox(request, id):
+    message = request.user.profile.sender.get(id=id)
+
+    context = {'message': message}
+    return render(request, 'user_outbox.html', context)
