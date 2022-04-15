cfb07df1470b74177c7bc4d9f05d7f895df58d04
Michael Tillman <mnamllit@gmail.com>
2022-04-15 13:35:07 -0500

    Organized imports

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from django.views import generic
-
-
 from .models import Post
 from .forms import CustomPostForm
 
