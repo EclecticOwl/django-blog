eeee1ddacd19d1a6bedc4fff89a4bf44287afe4b
Michael Tillman <mnamllit@gmail.com>
2022-04-15 18:05:27 -0500

    update my post list to use request factory.

--- posts/testing/test_views.py
+++ posts/testing/test_views.py
@@ -64,24 +64,15 @@ class AllPostListTest(TestCase):
         self.assertTrue(response.context['page_obj'])
         self.assertEqual(len(response.context['page_obj']), 4)
 
-
 class MyPostListViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        User.objects.create(
-            username='bob',
-            password='kjlekjxlKe13i'
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='bob', password='test'
         )
-        user = User.objects.get(id=1)
-        user.set_password('kjlekjxlKe13i')
-        user.save()
-
-        cls.user = 'bob'
-        cls.password = 'kjlekjxlKe13i'
+        profile = Profile.objects.get(id=1)
 
         num_posts = 10
-
-        profile = Profile.objects.get(id=1)
 
         for i in range(num_posts):
             Post.objects.create(
@@ -89,72 +80,26 @@ class MyPostListViewTest(TestCase):
                 description=f'{i}',
                 content=f'{i}',
             )
+    def test_details(self):
+        # Check url for desired location
+        request = self.factory.get('/posts/my-posts/')
+        request.user = self.user
+        response = views.MyPostListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
 
-    def test_if_redirect_if_anon(self):
-        response = self.client.get('/posts/my-posts/')
+        request = self.factory.get(reverse('my-posts'))
+        # Login Restrict Check
+        request.user = AnonymousUser()
+        response = views.MyPostListView.as_view()(request)
         self.assertEqual(response.status_code, 302)
-    
-    def test_view_url_exists_at_desired_location(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get('/posts/my-posts/')
+        # General Testing
+        request.user = self.user
+        response = views.MyPostListView.as_view()(request)
         self.assertEqual(response.status_code, 200)
-    
-    def test_url_access_by_name(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get(reverse('my-posts'))
-        self.assertEqual(response.status_code, 200)
-    
-    def test_correct_template(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get(reverse('my-posts'))
-        self.assertTemplateUsed(response, 'my_posts.html')
-    
-    def test_if_correct_num_posts_displayed(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get(reverse('my-posts'))
-        self.assertTrue(response.context['object_list'])
-        self.assertEqual(len(response.context['object_list']), 10)
+        self.assertEqual(response.template_name[0], 'my_posts.html')
 
+        
 
-class PostUpdateViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        User.objects.create(
-            username='bob',
-        )
-        user = User.objects.get(id=1)
-        user.set_password('kjlekjxlKe13i')
-        user.save()
-
-        cls.user = 'bob'
-        cls.password = 'kjlekjxlKe13i'
-
-        profile = Profile.objects.get(id=1)
-
-        Post.objects.create(
-            owner=profile,
-            description='blaa',
-            content='blaa',
-        )
-    
-    def test_if_redirect_if_anon(self):
-        response = self.client.get('/posts/post/1/')
-        self.assertEqual(response.status_code, 302)
-    
-    def test_view_url_exists_at_desired_location(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get('/posts/post/1/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_correct_template(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get(reverse('post-detail', kwargs={'pk': 1}))
-        self.assertTemplateUsed(response, 'post_detail.html')
-    
-    def test_url_access_by_name(self):
-        self.client.login(username=self.user, password=self.password)
-        response = self.client.get(reverse('post-detail', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
 
 class PostUpdateViewTest(TestCase):
     def setUp(self):
