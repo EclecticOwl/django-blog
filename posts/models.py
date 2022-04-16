9c44139cd4addd91754d5c0ddf331a0c40287412
Michael Tillman <mnamllit@gmail.com>
2022-04-16 15:37:39 -0500

    Revamped styling for the index page and added media query for it.

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from core.models import Profile
 class Post(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
     description = models.CharField(max_length=100)
-    content = models.TextField(max_length=400)
+    content = models.CharField(max_length=400)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
